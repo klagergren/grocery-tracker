d5cb602206ba85fe6122e7e41b2bc0a76ade56cf
Krister <kalagergren@gmail.com>
2025-10-22 16:33:01 -0400

    Added getReceipts... still needs work

--- src/main/java/kware/grocery/core/controller/ReceiptController.java
+++ src/main/java/kware/grocery/core/controller/ReceiptController.java
@@ -1,5 +1,6 @@
 package kware.grocery.core.controller;
 
+import java.util.List;
 import kware.grocery.generated.api.ReceiptApi;
 import kware.grocery.generated.model.Receipt;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,11 @@ public class ReceiptController implements ReceiptApi {
   @Override
   public ResponseEntity<String> saveReceipt(Receipt receipt) {
     return ResponseEntity.ok("saved"); //TODO: Implement
+  }
+
+  @Override
+  public ResponseEntity<List<Receipt>> getReceipts() {
+    return ResponseEntity.ok(List.of(Receipt.builder().receiptLines(List.of("hi")).build()));
   }
 
 }
